3c719f298b2747fe48f2bc6401187c91c7d11d26
Fawzi Gharib <fawz11gharib@gmail.com>
2024-03-18 05:16:41 +0300

    Assignment Done

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import Nav from "./Nav";
 
 import hogs from "../porkers_data";
+import HogGrid from "./HogGrid";
+import FilterHogs from "./FilterHogs";
+import SortHog from "./SortHog";
 
 function App() {
+	const [filter, setFilter] = useState("All");
+	const [sort, setSort] = useState("none");
 	return (
 		<div className="App">
 			<Nav />
+			<FilterHogs filter={filter} onChangeFilter={setFilter} />
+			<SortHog sort={sort} onChangeSort={setSort} />
+			<HogGrid hogs={hogs} filter={filter} sort={sort} />
 		</div>
 	);
 }
